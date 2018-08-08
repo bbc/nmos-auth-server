b11ef88cb9367f5d5e18ff9dd1d183f9ff31ba67
Danny Meloy <danny.meloy@rd.bbc.co.uk>
2018-08-08 15:14:52 +0100

    Added example-oauth-server
    altered .gitignore

---
+++ example-oauth2-server/website/models.py
@@ -0,0 +1,54 @@
+import time
+from flask_sqlalchemy import SQLAlchemy
+from authlib.flask.oauth2.sqla import (
+    OAuth2ClientMixin,
+    OAuth2AuthorizationCodeMixin,
+    OAuth2TokenMixin,
+)
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(40), unique=True)
+
+    def __str__(self):
+        return self.username
+
+    def get_user_id(self):
+        return self.id
+
+    def check_password(self, password): #TODO - add password field to schema and validation
+        return password == 'valid'
+
+
+class OAuth2Client(db.Model, OAuth2ClientMixin):
+    __tablename__ = 'oauth2_client'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(
+        db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
+    user = db.relationship('User')
+
+
+class OAuth2AuthorizationCode(db.Model, OAuth2AuthorizationCodeMixin):
+    __tablename__ = 'oauth2_code'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(
+        db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
+    user = db.relationship('User')
+
+
+class OAuth2Token(db.Model, OAuth2TokenMixin):
+    __tablename__ = 'oauth2_token'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(
+        db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
+    user = db.relationship('User')
+
+    def is_refresh_token_expired(self):
+        expires_at = self.issued_at + self.expires_in * 2
+        return expires_at < time.time()
---
+++ example-oauth2-server/website/routes.py
@@ -0,0 +1,146 @@
+from flask import Blueprint, request, session
+from flask import render_template, redirect, jsonify, url_for
+import requests
+from requests.auth import HTTPBasicAuth
+from werkzeug.security import gen_salt
+from authlib.flask.oauth2 import current_token
+from authlib.specs.rfc6749 import OAuth2Error
+from .models import db, User, OAuth2Client
+from .oauth2 import authorization, require_oauth
+
+
+bp = Blueprint(__name__, 'home')
+
+
+def current_user():
+    if 'id' in session:
+        uid = session['id']
+        return User.query.get(uid)
+    return None
+
+@bp.route('/_add_numbers')
+def add_numbers():
+    a = request.args.get('a', 0, type=int)
+    b = request.args.get('b', 0, type=int)
+    return jsonify(result=a + b)
+
+@bp.route('/index')
+def index():
+    return render_template('index.html')
+
+
+@bp.route('/', methods=('GET', 'POST'))
+def home():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        user = User.query.filter_by(username=username).first()
+        if not user:
+            user = User(username=username)
+            db.session.add(user)
+            db.session.commit()
+        session['id'] = user.id
+        return redirect('/')
+    user = current_user()
+    if user:
+        clients = OAuth2Client.query.filter_by(user_id=user.id).all()
+    else:
+        clients = []
+    return render_template('home.html', user=user, clients=clients)
+
+
+@bp.route('/logout')
+def logout():
+    del session['id']
+    return redirect('/')
+
+
+@bp.route('/create_client', methods=('GET', 'POST'))
+def create_client():
+    user = current_user()
+    if not user:
+        return redirect('/')
+    if request.method == 'GET':
+        return render_template('create_client.html')
+    client = OAuth2Client(**request.form.to_dict(flat=True))
+    client.user_id = user.id
+    client.client_id = gen_salt(24)
+    if client.token_endpoint_auth_method == 'none':
+        client.client_secret = ''
+    else:
+        client.client_secret = gen_salt(48)
+    db.session.add(client)
+    db.session.commit()
+    return redirect('/')
+
+
+@bp.route('/request_token', methods=['GET','POST'])
+def request_token():
+    user = current_user()
+    if not user:
+        return redirect('/')
+    client = OAuth2Client.query.filter_by(user_id=user.id).first() #TODO - specific client
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        scope = request.form.get('scope')
+        grant_type = 'password'
+        client_id = client.client_id
+        client_secret = client.client_secret
+        headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+        data = {'scope' : scope, 'grant_type' : grant_type, 'username' : username, 'password' : password}
+        resp = requests.post('http://127.0.0.1:5000/oauth/token', auth=HTTPBasicAuth(client_id, client_secret), headers = headers, data = data).json()
+        print resp
+        if 'access_token' in resp.keys():
+            session['token'] = resp['access_token']
+    if 'token' in session.keys():
+        pass
+    else:
+        session['token'] = None
+    return render_template('request.html', user=user, client=client, token = session['token'])
+
+
+@bp.route('/oauth/authorize', methods=['GET', 'POST'])
+def authorize():
+    user = current_user()
+    if request.method == 'GET':
+        try:
+            grant = authorization.validate_consent_request(end_user=user)
+        except OAuth2Error as error:
+            return error.error
+        return render_template('authorize.html', user=user, grant=grant)
+    if not user and 'username' in request.form:
+        username = request.form.get('username')
+        user = User.query.filter_by(username=username).first()
+    if request.form['confirm']:
+        grant_user = user
+    else:
+        grant_user = None
+    return authorization.create_authorization_response(grant_user=grant_user)
+
+
+@bp.route('/oauth/token', methods=['POST'])
+def issue_token():
+    return authorization.create_token_response()
+
+
+@bp.route('/oauth/revoke', methods=['POST'])
+def revoke_token():
+    return authorization.create_endpoint_response('revocation')
+
+#route for certificate with public key
+@bp.route('/certs', methods=['GET'])
+def get_cert():
+    return '''
+-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDdlatRjRjogo3WojgGHFHYLugd
+UWAY9iR3fy4arWNA1KoS8kVw33cJibXr8bvwUAUparCwlvdbH6dvEOfou0/gCFQs
+HUfQrSDv+MuSUMAe8jzKE4qW+jK+xQU9a03GUnKHkkle+Q0pX/g6jXZ7r1/xAK5D
+o2kQ+X5xK9cipRgEKwIDAQAB
+-----END PUBLIC KEY-----
+'''
+
+@bp.route('/api/me')
+@require_oauth('profile')
+def api_me():
+    user = current_token.user
+    return jsonify(id=user.id, username=user.username)
