71729b743e695069558b99a843a8866e406878dc
Danny Meloy <danny.meloy@rd.bbc.co.uk>
2019-02-12 16:03:54 +0000

    removed tests from master (moved to own branch)

--- tests/test_placeholder.py
+++ tests/test_placeholder.py
@@ -10,36 +10,12 @@ from __future__ import print_function
 from __future__ import absolute_import
 import unittest
 
-from nmosoauth.auth_server.db_utils import drop_all
-from nmosoauth.auth_server.security_api import SecurityAPI
 
-
-class TestNmosOauth(unittest.TestCase):
-
-    def setUp(self):
-        self.api = SecurityAPI(None, None, 'TestConfig')
-        self.app = self.api.app
-        self.client = self.app.test_client()
-
-    def tearDown(self):
-        with self.app.app_context():
-            drop_all()
+class TestPlaceholder(unittest.TestCase):
 
     def test_placeholder(self):
         test = 'test'
         assert('test' in test)
-
-    def testRoutes(self):
-        rv = self.client.get('/')
-        self.assertEqual(rv.status_code, 200)
-        rv = self.client.get('/token')
-        self.assertEqual(rv.status_code, 405)
-        rv = self.client.get('/revoke')
-        self.assertEqual(rv.status_code, 405)
-        rv = self.client.get('/register_client')
-        self.assertEqual(rv.status_code, 302)
-        rv = self.client.get('/fetch_token')
-        self.assertEqual(rv.status_code, 302)
 
 
 if __name__ == '__main__':
