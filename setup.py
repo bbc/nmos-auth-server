fd3004483a8c924f0c58104439b90e46f862893c
Danny Meloy <danny.meloy@rd.bbc.co.uk>
2019-03-06 11:14:12 +0000

    pre-release version bump

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import setup
 
 # Basic metadata
 name = "nmos-oauth"
-version = "0.0.0"
+version = "0.0.1"
 description = "OAuth2 Server Implementation"
 url = 'https://github.com/bbc/rd-apmm-python-oauth'
 author = 'Danny Meloy'
