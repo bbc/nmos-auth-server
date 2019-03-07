21928c9cf8bc9db952864a9477f2931ba8b41d6e
Danny Meloy <danny.meloy@rd.bbc.co.uk>
2019-03-07 18:10:03 +0000

    adjusted tests for new x-nmos/oauth/ prefix

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -19,6 +19,8 @@ from nmosoauth.auth_server.security_api import User
 from data_for_tests import BEARER_TOKEN, TEST_PRIV_KEY
 from base64 import b64encode
 
+VERSION_ROOT = '/x-nmos/oauth/v1.0'
+
 
 class TestNmosOauth(unittest.TestCase):
 
@@ -52,30 +54,30 @@ class TestNmosOauth(unittest.TestCase):
     def testInitialRoutes(self):
 
         with self.client as client:
-            rv = client.get('/', follow_redirects=True)
+            rv = client.get(VERSION_ROOT + '/', follow_redirects=True)
             self.assertEqual(rv.status_code, 200)
-            rv = client.get('/token', follow_redirects=True)
+            rv = client.get(VERSION_ROOT + '/token/', follow_redirects=True)
             self.assertEqual(rv.status_code, 405)
-            rv = client.get('/revoke', follow_redirects=True)
+            rv = client.get(VERSION_ROOT + '/revoke/', follow_redirects=True)
             self.assertEqual(rv.status_code, 405)
-            rv = client.get('/fetch_token')
+            rv = client.get(VERSION_ROOT + '/fetch_token/')
             self.assertEqual(rv.status_code, 302)
-            rv = client.get('/logout')
+            rv = client.get(VERSION_ROOT + '/logout/')
             self.assertEqual(rv.status_code, 302)
 
     def testBasicAuthRoutes(self):
 
         headers = self.auth_headers(self.mockUser)
         with self.client as client:
-            rv = client.get('/register_client')
+            rv = client.get(VERSION_ROOT + '/register_client/')
             self.assertEqual(rv.status_code, 401)
 
-            rv = client.get('/register_client', headers=headers)
+            rv = client.get(VERSION_ROOT + '/register_client/', headers=headers)
             self.assertEqual(rv.status_code, 200)
 
             wrongUser = self.createMockUser("bob", "pass")
             headers = self.auth_headers(wrongUser)
-            rv = client.get('/register_client', headers=headers)
+            rv = client.get(VERSION_ROOT + '/register_client/', headers=headers)
             self.assertEqual(rv.status_code, 401)
 
     @mock.patch("nmosoauth.auth_server.security_api.render_template")
@@ -85,16 +87,16 @@ class TestNmosOauth(unittest.TestCase):
 
         mockTemplate.return_value = "test"
 
-        with self.client.post('/', data=dict(username="", password="")):
+        with self.client.post(VERSION_ROOT + '/home/', data=dict(username="", password="")):
             mockTemplate.assert_called_with(
                 'home.html', clients=None, message='Please Fill In Both Username and Password.', user=None)
-        with self.client.post('/', data=dict(username="steve", password="wrongpassword")):
+        with self.client.post(VERSION_ROOT + '/home/', data=dict(username="steve", password="wrongpassword")):
             mockTemplate.assert_called_with(
                 'home.html', clients=None, message='Invalid Password. Try Again.', user=None)
-        with self.client.post('/', data=dict(username="steve", password="password")) as rv:
+        with self.client.post(VERSION_ROOT + '/home/', data=dict(username="steve", password="password")) as rv:
             self.assertEqual(rv.status_code, 200)
         mockUser.query.filter_by.return_value.first.return_value = None
-        with self.client.post('/', data=dict(username="steve", password="password")):
+        with self.client.post(VERSION_ROOT + '/home/', data=dict(username="steve", password="password")):
             mockTemplate.assert_called_with(
                 'home.html', clients=None, message='That username is not recognised. Please signup.', user=None)
 
