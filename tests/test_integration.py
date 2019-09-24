4c2a999719baea647a32b4123c9cc6a3458eac2b
Danny Meloy <danny.meloy@bbc.co.uk>
2019-09-24 10:09:25 +0100

    fix tests

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -20,8 +20,8 @@ from werkzeug.exceptions import HTTPException
 
 from nmoscommon.logger import Logger
 from nmosauth.auth_server.db_utils import drop_all
+from nmosauth.auth_server.models import AdminUser
 from nmosauth.auth_server.security_api import SecurityAPI
-from nmosauth.auth_server.security_api import User
 from nmos_auth_data import TEST_PRIV_KEY
 from base64 import b64encode
 
@@ -30,7 +30,7 @@ TEST_USERNAME = 'steve'
 TEST_PASSWORD = 'password'
 
 
-class TestNmosAuth(unittest.TestCase):
+class TestNmosAuthServer(unittest.TestCase):
 
     def setUp(self):
         self.api = SecurityAPI(logger=Logger("testing"), nmosConfig=None,
@@ -42,7 +42,7 @@ class TestNmosAuth(unittest.TestCase):
         self.user_id = 0
         self.testUser = self.createUser(TEST_USERNAME, TEST_PASSWORD)
         # Boilerplate for mocking out Basic Auth user for the whole class
-        patcher = mock.patch("nmosauth.auth_server.basic_auth.User")
+        patcher = mock.patch("nmosauth.auth_server.basic_auth.AdminUser")
         self.mockBasicUser = patcher.start()
         self.mockBasicUser.query.filter_by.return_value.first.return_value = self.testUser
         self.addCleanup(patcher.stop)
@@ -52,7 +52,7 @@ class TestNmosAuth(unittest.TestCase):
             drop_all()
 
     def createUser(self, username, password):
-        user = User()
+        user = AdminUser()
         user.username = username
         user.password = password
         user.id = self.user_id = self.user_id + 1
@@ -85,18 +85,18 @@ class TestNmosAuth(unittest.TestCase):
                 rv = client.post(VERSION_ROOT + '/authorize')
                 self.assertEqual(http_error.exception.code, 401)
 
-    @mock.patch("nmosauth.auth_server.security_api.getUser")
-    def testBasicAuthRoutes(self, mockGetUser):
+    @mock.patch("nmosauth.auth_server.security_api.getAdminUser")
+    def testBasicAuthRoutes(self, mockGetAdminUser):
 
-        mockGetUser.return_value = self.testUser
+        mockGetAdminUser.return_value = self.testUser
         headers = self.auth_headers(self.testUser)
         with self.client as client:
-            # Posting to Register client returns 200 status code
+            # Get /register_client returns 200 status code
             rv = client.get(VERSION_ROOT + '/register_client/', headers=headers)
             self.assertEqual(rv.status_code, 200)
-            mockGetUser.assert_called_with(self.testUser.username)
+            mockGetAdminUser.assert_called_with(self.testUser.username)
 
-            # Posting to register client with incorrect credentials returns Unauthorized
+            # Get /register_client with incorrect credentials returns Unauthorized
             wrongUser = self.createUser("bob", "pass")
             headers = self.auth_headers(wrongUser)
             with self.assertRaises(HTTPException) as http_error:
@@ -104,11 +104,11 @@ class TestNmosAuth(unittest.TestCase):
                 self.assertEqual(http_error.exception.code, 401)
 
     @mock.patch("nmosauth.auth_server.security_api.render_template")
-    @mock.patch("nmosauth.auth_server.security_api.User")
-    def testLogin(self, mockUser, mockTemplate):
+    @mock.patch("nmosauth.auth_server.security_api.getAdminUser")
+    def testLogin(self, mockGetAdminUser, mockTemplate):
 
         mockTemplate.return_value = "test"
-        mockUser.query.filter_by.return_value.first.return_value = self.testUser
+        mockGetAdminUser.return_value = self.testUser
 
         with self.client.post(VERSION_ROOT + '/login/', data=dict(username="", password="")):
             mockTemplate.assert_called_with(
@@ -121,7 +121,7 @@ class TestNmosAuth(unittest.TestCase):
         with self.client.post(VERSION_ROOT + '/login/', data=dict(username="steve", password="password")) as rv:
             self.assertEqual(rv.status_code, 302)
 
-        mockUser.query.filter_by.return_value.first.return_value = None
+        mockGetAdminUser.return_value = None
         with self.client.post(VERSION_ROOT + '/login/', data=dict(username="steve", password="password")):
             mockTemplate.assert_called_with(
                 'login.html', message='That username is not recognised. Please signup.')
@@ -138,8 +138,8 @@ class TestNmosAuth(unittest.TestCase):
         with self.client.post(VERSION_ROOT + '/signup', data=signup_data) as rv:
             self.assertEqual(rv.status_code, 302)
             with self.app.app_context():
-                self.assertEqual(self.testUser.username, User.query.get(1).username)
-                self.assertEqual(self.testUser.password, User.query.get(1).password)
+                self.assertEqual(self.testUser.username, AdminUser.query.get(1).username)
+                self.assertEqual(self.testUser.password, AdminUser.query.get(1).password)
 
         # Register Client
         register_data = {
